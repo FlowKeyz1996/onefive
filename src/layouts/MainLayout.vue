b5252b88c8c7ab8dfea3aa68737f38ad334be793
flowkeyz1996 <jonielsanni@gmail.com>
2022-03-30 13:39:24 +0100

    'onefivecommit'

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,181 @@
+<template>
+  <q-layout class="hue" view="lHh Lpr lFf">
+    <div class="nav container">
+
+      <div class="logo text-h5 q-ml-xl">
+      <span class="text-primary text-bold">ONE</span>FIVE
+      </div>
+
+      <nav>
+        <ul>
+          <q-tabs active-color="primary">
+            <q-route-tab to="/" label="Home" />
+            <q-route-tab class="text-" to="/page2" label="About Us" />
+            <q-route-tab class="text-" to="/page3" label="services" />
+            <q-route-tab class="text-" to="/page3" label="Contact Us" />
+
+          </q-tabs>
+        </ul>
+      </nav>
+
+      <nav>
+        <ul>
+          <q-tabs>
+            <q-route-tab class="text-info info text-primary" to="/page1" label="Log in" />
+            <q-route-tab class="text-white white bg-primary q-px-lg q-py-sm q-mr-xl" to="/page2" label="Sign Up" />
+          </q-tabs>
+        </ul>
+      </nav>
+
+      <q-btn
+          flat
+          dense
+          class="hide"
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+      />
+      <q-drawer
+      side="right"
+      v-model="leftDrawerOpen"
+      bordered
+      :breakpoint="1208"
+      >
+        <q-list>
+          <q-item-label
+          header
+        >
+          Essential Links
+        </q-item-label>
+
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+        </q-list>
+      </q-drawer>
+
+    </div>
+
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from 'components/EssentialLink.vue'
+
+const linksList = [
+  {
+    title: 'Docs',
+    caption: 'quasar.dev',
+    icon: 'school',
+    link: 'https://quasar.dev'
+  },
+  {
+    title: 'Github',
+    caption: 'github.com/quasarframework',
+    icon: 'code',
+    link: 'https://github.com/quasarframework'
+  },
+  {
+    title: 'Discord Chat Channel',
+    caption: 'chat.quasar.dev',
+    icon: 'chat',
+    link: 'https://chat.quasar.dev'
+  },
+  {
+    title: 'Forum',
+    caption: 'forum.quasar.dev',
+    icon: 'record_voice_over',
+    link: 'https://forum.quasar.dev'
+  },
+  {
+    title: 'Twitter',
+    caption: '@quasarframework',
+    icon: 'rss_feed',
+    link: 'https://twitter.quasar.dev'
+  },
+  {
+    title: 'Facebook',
+    caption: '@QuasarFramework',
+    icon: 'public',
+    link: 'https://facebook.quasar.dev'
+  },
+  {
+    title: 'Quasar Awesome',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev'
+  }
+];
+
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    EssentialLink
+  },
+
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+.q-tab__indicator {
+    background: #FED12E !important;
+}
+.q-drawer-container{
+  display: none;
+}
+.hue{
+    box-shadow: -1px 0 4px hsl(190, 64%, 15%, 0.15);
+
+}
+  .nav{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 70px;
+
+  }
+
+  .hide{
+    display: none;
+  }
+
+  @media(max-width:768px){
+    .hide{
+    display: block;
+    position: absolute;
+    /* top: 1px; */
+    z-index: 1000;
+    right: 20px;
+  }
+
+  .q-drawer-container{
+  display: block;
+}
+
+  nav{
+    display: none;
+  }
+  }
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,225 @@
+<template>
+  <q-page class="bg-primary background">
+    <section>
+    <div class="row">
+     <div class=" col q-pt-xl q-ma-xl">
+
+      <div class="text-h3 text-secondary q-pb-lg hero_info">
+        <span class="text-secondary text-bold">ONE</span>FIVE.COM
+      </div>
+      <div>
+        <p class="text-secondary text-body1 hero_info_para">Get your freelance service for nothing less than 1500</p>
+      </div>
+       <q-input
+        color="black"
+        class="bg-secondary insert"
+        filled
+        placeholder="enter your email here"
+             >
+        <template v-slot:prepend>
+          <q-icon name="email" />
+        </template>
+        <template v-slot:append>
+          <q-btn  class="btn" label="Get Started"></q-btn>
+        </template>
+      </q-input>
+      <div class= "q-mt-xl q-py-xl app_logo_container">
+       <p class="text-body2 text-secondary">Download the  ONEFIVE app</p>
+       <q-img src="icons/images/Group (2).png" class="app_image"></q-img>
+       <q-img src="/icons/images/Badge.png" class="app_image q-ml-md" ></q-img>
+       </div>
+      </div>
+      <div class="col image_container">
+        <q-img alt="hero-image" src="/icons/images/Group 153.png" class="hero_image"></q-img>
+
+      </div>
+
+    </div>
+
+  </section>
+  </q-page>
+
+  <div class="services_container">
+  <div class="text-h3 text-primary q-pa-md q-mt-xl text-bold">Our Services</div>
+  <div class="text-body1 services_para">We provide services to you based on your budget from the comfort of your home</div>
+   </div>
+   <div class="services_links q-pa-xl ">
+      <p>All</p>
+      <p>Plumbing</p>
+      <p>Electrian</p>
+      <p>Remodelling</p>
+      <p>Appliance-Repair</p>
+      <p>Cleaning</p>
+   </div>
+  <div class="q-pa-md q-ma-xl">
+
+    <div class="row">
+      <div class="col services_img_container">
+        <q-img src="/icons/images/Rectangle 23.png" class="services_image"> </q-img>
+       <p class="text-white text-body1 paragraph">Carpentry Services</p>
+       <q-btn color="primary" label="Primary" class="services_btn"/>
+      </div>
+      <div class="col services_img_container">
+        <q-img src="/icons/images/Rectangle 23.png"> </q-img>
+        <p class="text-white text-body1 paragraph"> Car wash Services</p>
+        <q-btn color="primary" label="Primary" class="services_btn"/>
+      </div>
+      <div class="col services_img_container">
+        <q-img src="/icons/images/Rectangle 23.png"> </q-img>
+         <p class="text-white text-body1 paragraph">Cleaning Services</p>
+        <q-btn color="primary" label="Primary" class="services_btn"/>
+      </div>
+    </div>
+
+  </div>
+  <section class=" sect_container ">
+
+    <div class="text-h3 text-primary text-bold work_header">
+      How it works
+      <p class="text-body1 text-secondary q-py-lg work_para">lorem isum dolor sit amet, consectectur ornare dolor volutpat</p>
+    </div>
+    <div class="padded">
+       <div class="text-h3">
+        how are you doing and how is everything
+      </div>
+      <div>
+        <q-img src="/icons/images/Rectangle.png"></q-img>
+      </div>
+     </div>
+
+  </section>
+  <section>
+
+  </section>
+
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PageIndex'
+})
+</script>
+
+
+
+<style scoped>
+.btn{
+  background-color:#2F4858;
+  color:white;
+}
+.hero_info_para{
+  max-width:45%;
+  font-size:1.2rem;
+}
+.app_image{
+  width:110px;
+
+}
+.app_logo_container{
+  margin-top:5rem;
+}
+/*.image_container{
+  padding-top:5rem;
+  background-color:#ffffff;
+  border-top-left-radius:2000px;
+  border-top-right-radius:2000px;
+  height:600px;
+  margin-top:120px;
+  margin-right:20px;
+  bottom:80%;
+  position:relative;
+}*/
+.hero_info{
+  padding-top:5rem;
+}
+.hero_image{
+ /* display:flex;
+  justify-content:center;
+  align-items:center;
+  margin-top:-60px;
+  margin-right:-20px;
+  align-items:center;
+  padding:5rem 3rem;*/
+  height:500px;
+  position:relative;
+  top:28%;
+}
+.insert{
+ max-width:85%;
+ border-radius:10px;
+}
+.services_container{
+  margin-top:10rem;
+  display:flex;
+  flex-direction:column;
+  align-items:center;
+  justify-content:center;
+}
+.services_para{
+  color:#2F4858;
+  font-size:1.5rem;
+  max-width:60%;
+  text-align:center;
+  line-height:1.5rem;
+}
+.services_links{
+  align-items:center;
+  justify-content:center;
+  display:flex;
+  color:#2F4858;
+}
+.services_links p{
+  margin:3rem;
+  padding:.5rem;
+  font-size:1rem;
+
+
+
+}
+
+.services_links p:hover{
+  background-color:#1abc9c;
+  border-radius:5px;
+  color:#ffff;
+
+  cursor:pointer;
+
+}
+.services_img_container{
+  padding:1rem;
+}
+
+.services_btn{
+  margin-top:2px;
+  position:relative;
+  bottom:20%;
+  left:15%;
+  background-color:black;
+}
+.paragraph{
+  position:relative;
+  bottom:30%;
+  left:15%;
+}
+.work_header{
+  margin:5rem;
+  padding:5rem;
+}
+.work_para{
+  font-size:1.2rem;
+  max-width:40%;
+}
+.sect_container{
+  background-color:#2F4858;
+}
+.padded{
+  display:flex;
+  flex-direction:row;
+  align-items:center;
+  justify-content:center;
+}
+
+
+</style>
